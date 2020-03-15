0bb66021c9005db663ef82a272e3ca6260962852
Kamil Gacek <kamil.gacek@atendesoftware.pl>
2020-03-15 13:45:49 +0100

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by Kamil on 15/03/2020.
+//  Copyright © 2020 Kamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
